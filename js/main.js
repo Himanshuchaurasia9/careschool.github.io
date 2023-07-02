fd4661373f65c7af584e83767c3b6e4ab6c262f9
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-07-02 13:13:28 +0530

    Care English School
    Care English School Website Frontend.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+
+
+
+
+
+
