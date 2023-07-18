99f6959342bf82c538098608e4902c3ea8cc67bb
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-07-18 12:33:58 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,22 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
 
 
 
+ document.addEventListener('contextmenu', (e) => e.preventDefault());
 
+ function ctrlShiftKey(e, keyCode) {
+   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+ }
+
+ document.onkeydown = (e) => {
+   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+   if (
+     event.keyCode === 123 ||
+     ctrlShiftKey(e, 'I') ||
+     ctrlShiftKey(e, 'J') ||
+     ctrlShiftKey(e, 'C') ||
+     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+   )
+     return false;
+ };
 
 
